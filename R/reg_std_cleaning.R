46e284e910de08c9dec955d82ba307e34b79b28b
youngjaewon <youngjaewon91@gmail.com>
2020-03-13 14:30:25 -0700

    reg_std_cleaning

--- R/reg_std_cleaning.R
+++ R/reg_std_cleaning.R
@@ -3,8 +3,36 @@ library(xml2)
 library(rvest)
 library(stringr)
 
-input.URL <- "http://www.gbig.org/buildings/8596/details"
-page <- xml2::read_html(input.URL)
-index <- page %>% html_nodes(xpath = '//*[@class="media feed-event project"]//*[@class="event"]') %>% html_text() %>% grep(pattern="LEED") %>% max
-reg.std <- page %>% html_nodes(xpath = '//*[@class="media feed-event project"]//*[@class="event"]') %>% html_text() %>% (function(x){x[index]})
-reg.date <- page %>% html_nodes(xpath = '//*[@class="media feed-event project"]//*[@class="date"]') %>% html_text() %>% (function(x){x[index]})
+df <- read.csv("C:/Users/ywon7/Desktop/GitHub/leed/data/activity_building_unique_geocoded_cleaned.csv")
+df <- df[!is.na(df$building.URL),]
+
+df$reg.std <- as.character(df$reg.std)
+df$reg.date <- as.character(df$reg.date)
+
+target.idx <- grep("LEED",df$reg.std, invert=TRUE)
+
+building.URL <- df$building.URL
+
+df$new.reg.std <- NA
+df$new.reg.date <- NA
+
+for(i in target.idx){
+  input.URL <- building.URL[i] %>% as.character
+  page <- xml2::read_html(input.URL)
+  index <- page %>% html_nodes(xpath = '//*[@class="media feed-event project"]//*[@class="event"]') %>% html_text() %>% grep(pattern="LEED") %>% max
+  reg.std <- page %>% html_nodes(xpath = '//*[@class="media feed-event project"]//*[@class="event"]') %>% html_text() %>% (function(x){x[index]})
+  reg.date <- page %>% html_nodes(xpath = '//*[@class="media feed-event project"]//*[@class="date"]') %>% html_text() %>% (function(x){x[index]})
+  
+  df$new.reg.std[i] <- reg.std
+  df$new.reg.date[i] <- reg.date
+  
+  if(i %% 100 == 0){cat("100..")}
+}
+
+df$reg.std[!is.na(df$new.reg.std)] <- df$new.reg.std[!is.na(df$new.reg.std)]
+df$reg.date[!is.na(df$new.reg.date)] <- df$new.reg.date[!is.na(df$new.reg.date)]
+
+df$new.reg.date <- NULL
+df$new.reg.std <- NULL
+
+write.csv(df, file = "C:/Users/ywon7/Desktop/GitHub/leed/data/final_before_cert.csv")
